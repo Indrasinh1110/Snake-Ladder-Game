20bed7c5da0aa81db89b78734506a0a98e9ba81f
Indrasinh <indrasinhmahera1110@gmail.com>
2023-07-20 00:22:53 +0530

    second commit

--- script.js
+++ script.js
@@ -1,13 +1,49 @@
-console.log("Welcome to Snake Ladder");
 let tog = 1;
 let p1sum = 0;
 let p2sum = 0;
-let gameon = true;
+let gameOn = true;
 let win = new Audio("./Audio/winner_snake.mp3");
 let snake = new Audio("./Audio/snake.wav");
 let ladder = new Audio("./Audio/ladder.wav");
+let diceAudio = new Audio("./Audio/dice.mp3");
+let player1, player2;
 var viewportWidth = window.innerWidth;
 
+function naming() {
+    player1 = document.getElementById('name1').value;
+    player2 = document.getElementById('name2').value;
+
+    document.getElementById('turn').innerText = player1 + "'s Turn";
+    document.getElementById('turn').style.color = 'red';
+}
+
+function rolling() {
+    if (gameOn) {
+        diceAudio.play();
+        var dice = document.getElementById('dice');
+        var interval = setInterval(changeDiceNumber, 50);
+        setTimeout(function () {
+            clearInterval(interval);
+            var num = Math.floor(Math.random() * 6) + 1;
+            dice.src = './Images/' + num + '.png';
+            tog = tog + 1;
+            if (tog % 2 !== 0) {
+                document.getElementById('turn').innerText = player1 + "'s Turn";
+                document.getElementById('turn').style.color = 'red';
+                play('p1', 'p1sum', num);
+            } else if (tog % 2 === 0) {
+                document.getElementById('turn').innerText = player2 + "'s Turn";
+                document.getElementById('turn').style.color = 'yellow';
+                play('p2', 'p2sum', num);
+            }
+        }, 750);
+    }
+}
+function changeDiceNumber() {
+    var dice = document.getElementById('dice');
+    var num = Math.floor(Math.random() * 6) + 1; // Generate random number for dice face
+    dice.src = './Images/' + num + '.png';
+}
 function play(player, pSum, num) {
     let sum;
     if (pSum === 'p1sum') {
@@ -139,11 +175,11 @@ function play(player, pSum, num) {
     }
     document.querySelector('.' + player).style.transition = 'linear all 0.5s';
     if (sum < 10) {
-        if(viewportWidth<=600){
+        if (viewportWidth <= 600) {
             document.querySelector('.' + player).style.left = (sum - 1) * 40 + 'px';
-            document.querySelector('.' + player).style.top = 360+ 'px';
+
         }
-        else{
+        else {
             document.querySelector('.' + player).style.left = (sum - 1) * 50 + 'px';
         }
     } else if (sum === 100) {
@@ -159,25 +195,23 @@ function play(player, pSum, num) {
             document.querySelector('.winner').innerText = player2 + ' is Won';
             document.querySelector('.winner').style.color = 'yellow';
         }
-        gameon = false;
-    } else 
-    {
-        let numArr = Array.from(String(sum));
-        let n1 = eval(numArr.shift());
-        let n2 = eval(numArr.pop());
+        gameOn = false;
+    } else {
+        let n1 = Math.floor(sum / 10);
+        let n2 = (sum % 10);
         if (viewportWidth <= 600) {
             if (n1 % 2 !== 0) {
                 if (n2 === 0) {
                     document.querySelector('.' + player).style.left = 9 * 40 + 'px';
-                    document.querySelector('.' + player).style.top = 360 - n1 * 40 + 'px';
+                    document.querySelector('.' + player).style.top = 400 - n1 * 40 + 'px';
                 } else {
                     document.querySelector('.' + player).style.left = (10 - n2) * 40 + 'px';
-                    document.querySelector('.' + player).style.top = 360 - n1 * 40+ 'px';
+                    document.querySelector('.' + player).style.top = 360 - n1 * 40 + 'px';
                 }
             } else if (n1 % 2 === 0) {
                 if (n2 === 0) {
                     document.querySelector('.' + player).style.left = 0 + 'px';
-                    document.querySelector('.' + player).style.top = 360 + -n1 * 40 + 'px';
+                    document.querySelector('.' + player).style.top = 400 + -n1 * 40 + 'px';
                 } else {
                     document.querySelector('.' + player).style.left = (n2 - 1) * 40 + 'px';
                     document.querySelector('.' + player).style.top = 360 - n1 * 40 + 'px';
@@ -188,7 +222,7 @@ function play(player, pSum, num) {
             if (n1 % 2 !== 0) {
                 if (n2 === 0) {
                     document.querySelector('.' + player).style.left = 9 * 50 + 'px';
-                    document.querySelector('.' + player).style.top = 450 - n1 * 50 + 'px';
+                    document.querySelector('.' + player).style.top = 500 - n1 * 50 + 'px';
                 } else {
                     document.querySelector('.' + player).style.left = (10 - n2) * 50 + 'px';
                     document.querySelector('.' + player).style.top = 450 - n1 * 50 + 'px';
@@ -196,7 +230,7 @@ function play(player, pSum, num) {
             } else if (n1 % 2 === 0) {
                 if (n2 === 0) {
                     document.querySelector('.' + player).style.left = 0 + 'px';
-                    document.querySelector('.' + player).style.top = 450 + -n1 * 50 + 'px';
+                    document.querySelector('.' + player).style.top = 500 + -n1 * 50 + 'px';
                 } else {
                     document.querySelector('.' + player).style.left = (n2 - 1) * 50 + 'px';
                     document.querySelector('.' + player).style.top = 450 - n1 * 50 + 'px';
@@ -206,49 +240,6 @@ function play(player, pSum, num) {
     }
 }
 
-// Taking input as name
-let player1, player2;
-function naming() {
-    player1 = document.getElementById('name1').value;
-    player2 = document.getElementById('name2').value;
-
-    document.getElementById('turn').innerText = player1 + "'s Turn";
-    document.getElementById('turn').style.color = 'red';
-}
-let diceAudio = new Audio("./Audio/dice.mp3");
-
-// Rolling Dice
-function rolling() {
-    if (gameon) {
-        diceAudio.play();
-
-        var dice = document.getElementById('dice');
-        var interval = setInterval(changeDiceNumber, 50); // Interval for changing dice number
-        var rollTime = 500 + Math.random() * 500; // Random time for rolling (between 1 and 3 seconds)
-        setTimeout(function () {
-            clearInterval(interval); // Stop changing dice number
-            roll.onclick = rolling; // Enable click after rolling stops
-            var num = Math.floor(Math.random() * 6) + 1; // Generate random number for final dice face
-            dice.src = './Images/' + num + '.png';
-            if (tog % 2 !== 0) {
-                document.getElementById('turn').innerText = player1 + "'s Turn";
-                document.getElementById('turn').style.color = 'red';
-                play('p1', 'p1sum', num);
-            } else if (tog % 2 === 0) {
-                document.getElementById('turn').innerText = player2 + "'s Turn";
-                document.getElementById('turn').style.color = 'yellow';
-                play('p2', 'p2sum', num);
-            }
-            tog = tog + 1;
-        }, rollTime);
-    }
-}
-
-function changeDiceNumber() {
-    var dice = document.getElementById('dice');
-    var num = Math.floor(Math.random() * 6) + 1; // Generate random number for dice face
-    dice.src = './Images/' + num + '.png';
-}
 
 function reset() {
     document.getElementById('name1').value = '';
@@ -257,7 +248,7 @@ function reset() {
     document.getElementById('name2').placeholder = 'Player 2';
     player1 = '';
     player2 = '';
-    gameon = true;
+    gameOn = true;
     p1sum = 0;
     p2sum = 0;
     sum = 0;
